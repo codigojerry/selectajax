50b295b14f1af6901af726b3830179819cf74d95
jerry <jersonmajin@gmail.com>
2013-03-06 16:45:48 -0500

    comentario importante

--- proc.php
+++ proc.php
@@ -8,7 +8,7 @@ $res=mysql_query("select * from pais where cod_cont=".$q."",$con);
 
 ?>
 
-<select id="pais" onchange="myFunction2(this.value)">
+<select id="pais" onchange="myFunction2(this.value)"><!--cuando seleccionan un pais se ejecuta la funcion myFunction2() ubicada en el archivo index.php-->
 
 <option value="">Seleccione</option>
 <?php while($fila=mysql_fetch_array($res)){ ?>
